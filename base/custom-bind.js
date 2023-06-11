bbecc0cf3042dea0f17c5610a44692c51eed6897
Maksym Vashchuk <maximvashchuk@gmail.com>
2023-06-11 18:41:30 +0300

    custom bind function

---
+++ base/custom-bind.js
@@ -0,0 +1,24 @@
+const info = {
+    firstName: 'Max',
+    lastName: 'V'
+}
+
+const printInfo = function (city, zip, country, shortName) {
+    console.log(`I am ${this.firstName} ${this.lastName} live in ${city} (${zip}), ${country} (${shortName})`);
+}
+
+const callMethod = printInfo.bind(info, 'Vinnytsia', 21000);
+callMethod('Ukraine', 'UA');
+
+Function.prototype.myBind = function (context, ...args1) {
+    const fn = this;
+    return function (...args2) {
+        // fn.call(context, ...[...args1, ...args2]);
+
+        fn.apply(context, [...args1, ...args2])
+    }
+}
+
+const customCallMethod = printInfo.myBind(info, 'Vinnytsia', 21000);
+customCallMethod('Ukraine', 'UA');
+
