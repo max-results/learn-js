c88d5d417e8fd87547332f9cf056d610a5929c6d
Maksym Vashchuk <maximvashchuk@gmail.com>
2024-02-12 01:43:35 +0200

    play with data and readable

--- streams/counter-readable.js
+++ streams/counter-readable.js
@@ -1,4 +1,5 @@
 const stream  = require('node:stream');
+const timers = require('node:timers/promises')
 
 class Counter extends stream.Readable {
     constructor(opt) {
@@ -12,7 +13,7 @@ class Counter extends stream.Readable {
     // _read() буде викликано знову після кожного виклику this.push(dataChunk),
     // коли потік буде готовий прийняти більше даних
     // _read() може продовжувати читати з ресурсу та надсилати дані, доки readable.push() не поверне false
-    _read() {
+    async _read() {
         this._index += 1;
 
         if (this._index > this._max) {
@@ -21,12 +22,29 @@ class Counter extends stream.Readable {
             const buf = Buffer.from(`${this._index}`, 'utf8');
 
             console.log(`Added: ${this._index}. Could be added? `, this.push(buf));
+            // push повертає false, коли буфер буде заповнений.
+            // true - коли НЕ заповнений і додаткові порції даних можуть надсилатись
         }
     }
 }
 
-const counter = new Counter({ highWaterMark: 2 });
+const counter = new Counter();
 
+// Подія 'data' - спрацьовує кожного разу, коли є нові дані для читання
 counter.on('data', chunk => {
-    console.log(`Received: ${chunk.toString()}`);
+    console.log(`Data received: ${chunk.toString()}`);
 });
+
+
+// Подія 'readable' - спрацьовує коли є можливість прочитати дані без блокування
+counter.on('readable', () => {
+    // console.log(`readable`, counter.read(1)?.toString());
+    let chunk;
+    // Читаємо дані поки можливо без блокування
+    while ((chunk = counter.read()) !== null) {
+        console.log('Data received (readable event):', chunk.toString());
+    }
+});
+
+//
+// https://github.com/nodejs/node/blob/main/lib/internal/streams/readable.js#L548
